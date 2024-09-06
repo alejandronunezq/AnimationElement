e2e55d34288fe145d665d0bb90f8affe9d576529
alejandronunezq <202160003@ucc.mx>
2024-09-05 20:20:15 -0600

    Segundo commit

--- app/src/main/java/com/example/animationelement/MainActivity.java
+++ app/src/main/java/com/example/animationelement/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.animationelement;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private AnimationDrawable doggy;
+    private ImageView imagen;
+    private boolean isAnimating = true; // Variable para controlar el estado de la animación
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        imagen = (ImageView) findViewById(R.id.imagenPerrito);
+        imagen.setBackgroundResource(R.drawable.animacion_doggy);
+        doggy = (AnimationDrawable) imagen.getBackground();
+        doggy.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (isAnimating) {
+                doggy.stop();
+                isAnimating = false;
+            } else {
+                imagen.setBackgroundResource(R.drawable.animacion_doggy);
+                doggy = (AnimationDrawable) imagen.getBackground();
+                doggy.start();
+                isAnimating = true;
+            }
+        }
+        return super.onTouchEvent(event);
     }
 }
